cedea1602f74a6e140128a39838f0cbad2948323
PetraBakaric88 <petra.balagovic@gmail.com>
2022-05-08 13:56:33 +0200

    first excercise

---
+++ 1zadatak.dart
@@ -0,0 +1,11 @@
+String firstName = "Petra";
+String lastName = "Bakarić";
+num age = 33;
+num height = 1.68;
+
+void main() {
+  print(firstName);
+  print(lastName);
+  print(age);
+  print(height);
+}
